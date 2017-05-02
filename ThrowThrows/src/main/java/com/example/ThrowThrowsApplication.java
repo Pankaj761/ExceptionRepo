c4f62d110383e49930247de0fcc997cd36335af0
pkj\pkjuser <pankajkumarsingh761@gmail.com>
2017-05-02 20:46:17 +0530

    first commit
    first push

---
+++ ThrowThrows/src/main/java/com/example/ThrowThrowsApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowThrowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrowThrowsApplication.class, args);
+	}
+}
